a494f9683b61ecd95bf7e2688ebfd3a3ae5316cc
UglySquid <christine.wei.63@gmail.com>
2023-06-20 22:00:10 -0400

    code comments (sprites.py)

--- sprites.py
+++ sprites.py
@@ -1,7 +1,7 @@
 """
 date: june 19th, 2023
 name: christine wei and william yang
-description: this module is meant to contain all of the world data and set up the in-game world, along with the player
+description: this module is meant to contain all the world data and set up the in-game world, along with the player
 centered camera
 """
 
@@ -19,7 +19,7 @@ from settings import *
 # Set working directory to current directory so that it is easier to find files
 os.chdir(os.getcwd())
 
-
+# CLASSES
 class Tile(pygame.sprite.Sprite):
     """
     Generic class tile. Parent class for all the tile objects
@@ -94,42 +94,52 @@ class Sprites:
     """
 
     def __init__(self, screen, actions):
-        # visible sprites = seen ones that don't have collision, obstacles = collisions
+        """
+        Dunder function, initializes all the attributes
+        :param screen:
+        :param actions:
+        """
 
-        # get the screen sprites will be displayed on
+        # Initialize the player (PyCharm thing)
         self.player = None
+
+        # Get the screen sprites will be displayed on
         self.screen = screen
 
-        # sprite Groups
+        # SPRITE GROUPS
+        # All the sprites in this group so that we can update all the sprites at the same time
         self.sprite_group = CameraGroup()
+        # All the apples and their chests
         self.apple_sprites = pygame.sprite.Group()
+        # All sprites that must be checked for collision
         self.obstacle_sprites = pygame.sprite.Group()
+        # All the bot guards
         self.bot_group = pygame.sprite.Group()
-        self.chest_group = pygame.sprite.Group()
-        self.floor_items = pygame.sprite.Group()
+        # All bullet sprites
         self.bullet_sprites = pygame.sprite.Group()
 
         # set up sprites and create map
         self.setup(actions)
 
     def setup(self, actions):
-        # E - Entities
+        """
+        This is where the map is creates
+        :param actions:
+        :return:
+        """
+
+        # E - Entities and initializing
         #  initialize tmx data
         self.screen.fill((114, 117, 27))
         tmx_data = load_pygame('./data/tmx/fuki4.tmx')
 
-        # Map borders made with stone walls
-        # for x, y, surf in tmx_data.get_layer_by_name("Borders").tiles():
-        #     pos = (x * 32, y * 32)
-        #     Tile(position=pos, surface=surf, groups=[self.sprite_group, self.obstacle_sprites], z=LAYERS['Borders'])
-
-        # facility
+        # Create the facility
         for x, y, surf in tmx_data.get_layer_by_name("Facility").tiles():
             pos = (x * 32, y * 32)
             Tile(position=pos, surface=surf, groups=[self.sprite_group, self.obstacle_sprites],
                  z=LAYERS['Facility'])
 
-        # Facility Decorations
+        # Add facility Decorations
         for layer in ["Facility Deco", "Facility Deco 2"]:
             for x, y, surf in tmx_data.get_layer_by_name(layer).tiles():
                 pos = (x * 32, y * 32)
@@ -141,15 +151,15 @@ class Sprites:
         for obj in tmx_data.get_layer_by_name('Trees'):
             Trees((obj.x, obj.y), obj.image, [self.sprite_group, self.obstacle_sprites], obj.name)
 
+        # house layers
         for obj in tmx_data.get_layer_by_name('Houses'):
             City((obj.x, obj.y), obj.image, [self.sprite_group, self.obstacle_sprites], obj.name)
 
+        # chest layers
         for obj in tmx_data.get_layer_by_name('Chests'):
             Chest((obj.x, obj.y), obj.image, [self.sprite_group, self.obstacle_sprites, self.apple_sprites])
 
-        self.player = player.Player((1600, 1600), self.sprite_group, self.obstacle_sprites, self.bullet_sprites,
-                                    self.apple_sprites, self.screen)
-
+        # MAP BACKGROUND (THE GROUND)
         Tile(
             position=(0, 0),
             surface=pygame.image.load('./graphics/map_bg.png'),
@@ -157,10 +167,16 @@ class Sprites:
             z=LAYERS['Ground']
         )
 
-        # initialize AI guard entities based on the current level
+        # Create the player
+        self.player = player.Player((1600, 1600), self.sprite_group, self.obstacle_sprites, self.bullet_sprites,
+                                    self.apple_sprites, self.screen)
+
+        # Initialize AI guard entities based on the current level
+        # Initialize AI for level 1
         if actions["Level1"]:
             num_guards = 10
-        else:
+        # Initialize AI for level 2
+        elif actions["Level2"]:
             num_guards = 20
         for guard in range(num_guards):
             guard = bot.Bot((random.randint(400, 2800), random.randint(400, 2800)),
@@ -171,22 +187,26 @@ class Sprites:
 
             self.bot_group.add(guard)
 
-        # number of guards not working
-
     def run(self, dt, actions):
-        for sprite in self.bot_group:
-            if sprite.bot_dead:
-                Apple(sprite.position, [self.sprite_group, self.apple_sprites])
-                sprite.kill()
+        """
+        A - ACTIONS. Whate occurs in the loop
+        :param dt:
+        :param actions:
+        :return:
+        """
 
         # R - Refresh screen and update all sprites
         self.screen.fill('black')
         self.sprite_group.custom_draw(self.player)
 
+        # Get mouse position on the screen
         mouse_pos = list(pygame.mouse.get_pos())
+
+        # Calculate mouse position on the map
         mouse_pos[0] += self.sprite_group.offset.x
         mouse_pos[1] += self.sprite_group.offset.y
 
+        # Check if a chest has been clicked
         for chest in self.apple_sprites.sprites():
             if hasattr(chest, 'chest'):
                 if pygame.mouse.get_pressed()[0] and chest.chest_rect.collidepoint(mouse_pos):
@@ -198,8 +218,17 @@ class Sprites:
                 sprite.kill()
             self.player.dead = True
 
+        # Check for dead sprites
+        for sprite in self.bot_group:
+            if sprite.bot_dead:
+                Apple(sprite.position, [self.sprite_group, self.apple_sprites])
+                sprite.kill()
+
     def update(self):
-        # R - Refresh screen and draw all the sprites
+        """
+        R - Refresh screen and draw all the sprites
+        :return:
+        """
         self.sprite_group.draw(self.screen)
         self.bullet_sprites.draw(self.screen)
         self.bullet_sprites.update()
